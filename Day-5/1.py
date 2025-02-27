27767d185c5194c7961e07adf38df19fffb771d1
Dhruvi Modha <dhruvimodha1@gmail.com>
2025-02-27 23:40:45 +0530

    Day-5

--- Day-5/1.py
+++ Day-5/1.py
@@ -12,5 +12,5 @@ user_input = int(input("Enter a number: "))
 if user_input % 2 == 0:
     print(f"{user_input} is an even number.")
 else:
-    print(f"{user_input} is an odd number.")
+    print(f"{user_input} is an odd number.")  
 
