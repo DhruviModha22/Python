6c9c76e5ceafda38c191d32b969b83d6cfd432aa
Dhruvi Modha <dhruvimodha1@gmail.com>
2025-02-27 23:41:45 +0530

    Day-7

--- Day-7/1.py
+++ Day-7/1.py
@@ -1,3 +1,3 @@
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
-print(f"Hello, {last_name}, {first_name}!")
+print(f"Hello, {last_name}, {first_name}!")   
